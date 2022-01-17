34ea7f91e2de0b79a503132b99ccd780fa64b33b
anjitpariyar <anjitpariyar@gmail.com>
2022-01-17 13:19:48 +0545

    blog single page added

--- blog-single-page.php
+++ blog-single-page.php
@@ -1,7 +1,7 @@
 <?php
-$currentpage = 'home';
+$currentpage = 'other';
 include 'header.php';
-// include './template/nav.php';
+include './template/nav.php';
 ?>
 
 
@@ -9,20 +9,145 @@ include 'header.php';
 	
 	<section class="bg-white template--part">
 		<div class="section-rule">
-			<h2 class="section__title">“Connecting every student with the best instructors of the nation”</h2>
-			<img src="./gallery/card__img/img01.png" alt="" class="img-fluid">
-			<div class="para">
-				<p class="para">Primarily focused for secondary school and high school students, each subject is designed comprehensively to cater their academic needs. You’ll learn in variety of ways including pre- recorded video tutorials, live online classes, Mentee library, Discussion forums and many more.Mentored by top-notch instructors, you will not only prepare yourself from examination point of view, but get an in-depth knowledge of every subject material. </p>
+			<section class="main-content">
+				<article class="card--lg">
+					<div class="card--top">
+						<!-- image size is 640 * 420 -->
+						<div class="card--img">
+							<img src="./gallery/card__img/img08.png" alt=""/>
+						</div>
+						<div class="card--text">
+							<h2 class="section__title">“Connecting every student with the best instructors of the nation”</h2>
+							<p class="para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Accumsan tincidunt rutrum tempor, euismod tellus duis pellentesque nibh vulputate. Ullamcorper mus commodo, augue laoreet lectus vivamus. Phasellus mauris egestas faucibus sed. Nunc eget enim, pellentesque maecenas nulla vestibulum. Cursus dignissim maecenas id suspendisse at dictum cras tempor. Eget aliquet morbi tincidunt blandit sed eu, mus. Turpis arcu at turpis mauris, cursus sed integer fermentum. Etiam sodales in dolor faucibus fermentum viverra duis sed pellentesque. Tortor in libero scelerisque semper condimentum odio aenean. Quis diam ullamcorper aliquam proin elementum enim platea in mauris. Varius id quam volutpat viverra neque. Ultrices laoreet ornare in tincidunt. Commodo nam lobortis et, lacus felis. Ultrices laoreet ornare in tincidunt. </p>
+						</div>
+					</div>
+					<div class="card--body w-100 " style="display:none"  id="collapseExample">
+						<p class="para">
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum beatae laudantium quis hic autem excepturi a perspiciatis, deleniti quia ratione odio nihil, vel rerum similique! Quas iure provident beatae tempore?
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum beatae laudantium quis hic autem excepturi a perspiciatis, deleniti quia ratione odio nihil, vel rerum similique! Quas iure provident beatae tempore?
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum beatae laudantium quis hic autem excepturi a perspiciatis, deleniti quia ratione odio nihil, vel rerum similique! Quas iure provident beatae tempore?
+						</p>
+						<!-- its hidden initial -->
+					</div>
+					<div class="d-flex">
+								<p><span class="material-icons-outlined material-icons">visibility</span>322 Views</p>
+								<p><span class="material-icons material-icons-outlined">location_on</span>Banepa, Nepal</p>
+								<p>Last Updated: 8 May 2020</p>
+								<a href="#" title="share">
+										<svg xmlns="http://www.w3.org/2000/svg" width="19.334" height="19.334" viewBox="0 0 19.334 19.334">
+											<path id="network" d="M15.191,11.048a4.129,4.129,0,0,0-3.333,1.7l-3.77-1.886a4.087,4.087,0,0,0,.2-1.2,4.087,4.087,0,0,0-.2-1.2l3.77-1.886a4.126,4.126,0,1,0-.81-2.438,4.086,4.086,0,0,0,.2,1.2L7.476,7.229a4.143,4.143,0,1,0,0,4.876l3.77,1.886a4.086,4.086,0,0,0-.2,1.2,4.143,4.143,0,1,0,4.143-4.143Zm0-9.667a2.762,2.762,0,1,1-2.762,2.762A2.765,2.765,0,0,1,15.191,1.381ZM4.143,12.429A2.762,2.762,0,1,1,6.9,9.667a2.765,2.765,0,0,1-2.762,2.762Zm11.048,5.524a2.762,2.762,0,1,1,2.762-2.762A2.765,2.765,0,0,1,15.191,17.953Z" transform="translate(0)" fill="#3f8809"></path>
+										</svg>
+										<span>Share</span> 
 
-				<p>Mentee believes schooling and curriculum-based education is just not enough for today’s (next gen) kids. Variety of professional courses packaged from very basic to advanced level makes Mentee Marketplace a perfect place for every individual, be it a primary level student seeking to widen their skill base or a professional seeking to change their career or even just looking to fulfill their long-endeavored hobby. </p>
+									</a>
+								<div class="ml-auto">
+									
+								<a  class="card__link"  href="#!"  id="card__link--read"><svg xmlns="http://www.w3.org/2000/svg" width="14.727" height="14.727" viewBox="0 0 14.727 14.727">
+								<path id="Union_6" data-name="Union 6" d="M.127,10.749V2.379L0,2.389.127.8,10.079,0,9.951,1.592l-8.232.66v8.5Z" transform="translate(14.727 7.6) rotate(135)" fill="#313131" opacity="0.8"></path></svg>
+								<span>Read More</span>
+							</a>
+								</div>
+							</div>
 
-				<p>Having more than 15 years of experience in Nepal 's education agency market, the founders of Mentee obtained first-hand insight into Nepal's education system and how the existing system fell behind relative to the foreign education system.
-
-					The temptation of trying to fill up the gap was always there; do something to reach every student to the ends of remote villages, help them achieve their optimum potential, enabling enhanced lifestyle; do something to break the stereotypical biasness perceived in education available between metro cities and rural villages; and do something to escalate the literacy rate of Nepal.
-
-				This urge was then fueled by COVID 19 pandemic in 2020, which further jeopardized the current education system, and convinced the founders of the imminent need for an extensive online education system to enable every Nepali citizen, irrespective of age, sex, caste, religion and geographical location gain access to specialized and quality education at their fingertips. </p>
-			</div>
+				</article>
+			</section>
+			<section class="blog">
+				<h2 class="section__title">Latest Blogs</h2>
+				<div class="carousel">
+					<div class="item">
+						<div class="card">
+							<div class="card__img">
+								<img src="./gallery/card__img/img01.png" alt="">
+							</div>
+							<div class="card__body">
+								<h2 class="small__title">22 Nov, 2019</h2>
+								<h2 class="card__title"><a href="#">The 5 Biggest Mistake Veteran and Military Home Buyers Made</a></h2>
+								<p class="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos molestias sequi repudiandae cupiditate nemo placeat, omnis. Maiores est, repellendus suscipit.</p>
+								<a href="#" class="card__link" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="14.727" height="14.727" viewBox="0 0 14.727 14.727">
+									<path id="Union_6" data-name="Union 6" d="M.127,10.749V2.379L0,2.389.127.8,10.079,0,9.951,1.592l-8.232.66v8.5Z" transform="translate(14.727 7.6) rotate(135)" fill="#313131" opacity="0.8"/></svg>Read More
+								</a>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="card">
+							<div class="card__img">
+								<img src="./gallery/card__img/img01.png" alt="">
+							</div>
+							<div class="card__body">
+								<h2 class="small__title">22 Nov, 2019</h2>
+								<h2 class="card__title"><a href="#">The 5 Biggest Mistake Veteran and Military Home Buyers Made</a></h2>
+								<p class="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos molestias sequi repudiandae cupiditate nemo placeat, omnis. Maiores est, repellendus suscipit.</p>
+								<a href="#" class="card__link" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="14.727" height="14.727" viewBox="0 0 14.727 14.727">
+									<path id="Union_6" data-name="Union 6" d="M.127,10.749V2.379L0,2.389.127.8,10.079,0,9.951,1.592l-8.232.66v8.5Z" transform="translate(14.727 7.6) rotate(135)" fill="#313131" opacity="0.8"/></svg>Read More
+								</a>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="card">
+							<div class="card__img">
+								<img src="./gallery/card__img/img01.png" alt="">
+							</div>
+							<div class="card__body">
+								<h2 class="small__title">22 Nov, 2019</h2>
+								<h2 class="card__title"><a href="#">The 5 Biggest Mistake Veteran and Military Home Buyers Made</a></h2>
+								<p class="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos molestias sequi repudiandae cupiditate nemo placeat, omnis. Maiores est, repellendus suscipit.</p>
+								<a href="#" class="card__link" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="14.727" height="14.727" viewBox="0 0 14.727 14.727">
+									<path id="Union_6" data-name="Union 6" d="M.127,10.749V2.379L0,2.389.127.8,10.079,0,9.951,1.592l-8.232.66v8.5Z" transform="translate(14.727 7.6) rotate(135)" fill="#313131" opacity="0.8"/></svg>Read More
+								</a>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="card">
+							<div class="card__img">
+								<img src="./gallery/card__img/img01.png" alt="">
+							</div>
+							<div class="card__body">
+								<h2 class="small__title">22 Nov, 2019</h2>
+								<h2 class="card__title"><a href="#">The 5 Biggest Mistake Veteran and Military Home Buyers Made</a></h2>
+								<p class="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos molestias sequi repudiandae cupiditate nemo placeat, omnis. Maiores est, repellendus suscipit.</p>
+								<a href="#" class="card__link" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="14.727" height="14.727" viewBox="0 0 14.727 14.727">
+									<path id="Union_6" data-name="Union 6" d="M.127,10.749V2.379L0,2.389.127.8,10.079,0,9.951,1.592l-8.232.66v8.5Z" transform="translate(14.727 7.6) rotate(135)" fill="#313131" opacity="0.8"/></svg>Read More
+								</a>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="card">
+							<div class="card__img">
+								<img src="./gallery/card__img/img01.png" alt="">
+							</div>
+							<div class="card__body">
+								<h2 class="small__title">22 Nov, 2019</h2>
+								<h2 class="card__title"><a href="#">The 5 Biggest Mistake Veteran and Military Home Buyers Made</a></h2>
+								<p class="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos molestias sequi repudiandae cupiditate nemo placeat, omnis. Maiores est, repellendus suscipit.</p>
+								<a href="#" class="card__link" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="14.727" height="14.727" viewBox="0 0 14.727 14.727">
+									<path id="Union_6" data-name="Union 6" d="M.127,10.749V2.379L0,2.389.127.8,10.079,0,9.951,1.592l-8.232.66v8.5Z" transform="translate(14.727 7.6) rotate(135)" fill="#313131" opacity="0.8"/></svg>Read More
+								</a>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="card">
+							<div class="card__img">
+								<img src="./gallery/card__img/img01.png" alt="">
+							</div>
+							<div class="card__body">
+								<h2 class="small__title">22 Nov, 2019</h2>
+								<h2 class="card__title"><a href="#">The 5 Biggest Mistake Veteran and Military Home Buyers Made</a></h2>
+								<p class="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos molestias sequi repudiandae cupiditate nemo placeat, omnis. Maiores est, repellendus suscipit.</p>
+								<a href="#" class="card__link" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="14.727" height="14.727" viewBox="0 0 14.727 14.727">
+									<path id="Union_6" data-name="Union 6" d="M.127,10.749V2.379L0,2.389.127.8,10.079,0,9.951,1.592l-8.232.66v8.5Z" transform="translate(14.727 7.6) rotate(135)" fill="#313131" opacity="0.8"/></svg>Read More
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>	
 		</div>
+
 
 		
 	</section>
@@ -32,10 +157,62 @@ include 'header.php';
 </main>
 
 <?php
-// include 'footer.php';
+include 'footer.php';
 ?>
 
 <script type="text/javascript" src="./js/aboutus.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+
+		//collapse
+		$('#card__link--read').click(function(e){
+			e.preventDefault();
+			if($(this).hasClass('active')){
+				$(this).find('span').text("Read More");
+				$(this).removeClass('active');
+
+			}
+			else{
+				$(this).find('span').text("Read Less");
+				$(this).addClass('active');
+
+			}
+			$("#collapseExample").toggle(300);
+
+		})
+
+		//slider 
+		$(".blog .carousel").slick({
+		infinite: true,
+		lazyLoad: 'ondemand',
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows:true,
+		draggable:true,
+		prevArrow:"<button class=' slick-prev'><i class='material-icons'>keyboard_arrow_left</i></button>",
+		nextArrow:"<button class=' slick-next '><i class='material-icons'>keyboard_arrow_right</i></button>",
+		responsive: [
+		{
+			breakpoint: 840,
+			settings: {
+				slidesToShow: 2.2,
+				arrows:false,
+				infinite: false,
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 1.4,
+				arrows:false,
+				infinite: false,
+			}
+		}
+
+		]
+	});
+	})
+</script>
 
 
 </body>
